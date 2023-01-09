5b39f19060f1669396bc7a7226936a4d1454f4c9
wzyandi <wzyandi@gmail.com>
2023-01-09 11:37:14 +0800

    重置APP图标

--- LifeNote/Models/About.cs
+++ LifeNote/Models/About.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LifeNote.Models
+﻿namespace LifeNote.Models
 {
     internal class About
     {
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string MoreInfoUrl => "https://github.com/amerina/LifeNote";
+        public string Message => "This is a microservice architecture MAUI sample APP.";
     }
 }
